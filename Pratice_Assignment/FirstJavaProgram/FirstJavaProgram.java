9c553ab23ee994e51426effdb70165df27b71d2f
Itserge1 <sergecodefirst@gmail.com>
2023-08-22 16:10:41 -0400

    Initial commit

---
+++ Pratice_Assignment/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My Name is Coding Dojo");
+        System.out.println("I am 100 Year Old");
+        System.out.println("My Home Town is Burbank, CA");
+    }
+}
