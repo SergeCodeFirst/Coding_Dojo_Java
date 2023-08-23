828688ab97a62f141d9c45be4ad7c229113759ed
Itserge1 <sergecodefirst@gmail.com>
2023-08-23 19:56:57 -0400

    add variable

--- learn_inclass/HelloWorld.java
+++ learn_inclass/HelloWorld.java
@@ -1,5 +1,19 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello world!");
-    }
+        // System.out.println("Hello world!");
+        System.out.println("---------------------");
+        int num;
+        num = 4;
+        int age = 5;
+        String name = "Hellow";
+        boolean isBoy = true;
+        double price = 34.0;
+        char bloodType = 'B';
+        String message = "my name is: " + name + ", Blood type:" + bloodType + ", I'm " + age + "'s years old; And my Number is: " + num +". i'm a boy: " + isBoy + ", and i bought my food for $"+ price + ".";
+        System.out.println(message);
+        System.out.println(message.concat(" I'm Human."));
+        System.out.println(message.length());
+        
+        System.out.println("---------------------");
+    } 
 }
